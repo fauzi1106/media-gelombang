6211367a320c8c940d9e5ff85a506975d31b69be
maulakbaarr <mhmmdakbar454@gmail.com>
2026-06-07 14:16:59 +0800

    masalah pdf bismillahh habis ini beres ya allah

--- app/Http/Controllers/GelombangSubmissionController.php
+++ app/Http/Controllers/GelombangSubmissionController.php
@@ -20,35 +20,40 @@ class GelombangSubmissionController extends Controller
     // =========================
     // PROSES UPLOAD
     // =========================
-   public function store(Request $request)
-{
-    $request->validate([
-        'file' => 'required|mimes:pdf|max:2048',
-        'latihan_code' => 'required'
-    ], [
-        'file.required' => 'Silakan pilih file PDF terlebih dahulu.',
-        'file.mimes' => 'File harus berformat PDF.',
-        'file.max' => 'Ukuran file maksimal 2 MB.'
-    ]);
+    public function store(Request $request)
+    {
+        $request->validate([
+            'file' => 'required|mimes:pdf|max:2048',
+            'latihan_code' => 'required'
+        ], [
+            'file.required' => 'Silakan pilih file PDF terlebih dahulu.',
+            'file.mimes' => 'File harus berformat PDF.',
+            'file.max' => 'Ukuran file maksimal 2 MB.'
+        ]);
 
-    $file = $request->file('file');
+        $file = $request->file('file');
 
-    dd($file);
+        $fileName = $request->latihan_code . '_' . Auth::id() . '_' . time() . '.pdf';
 
-    $fileName = $request->latihan_code . '_' . Auth::id() . '_' . time() . '.pdf';
+        $result = $file->move(
+            public_path('submissions'),
+            $fileName
+        );
 
-    $file->move(public_path('submissions'), $fileName);
+        dd($result);
 
-    $path = 'submissions/' . $fileName;
+        $file->move(public_path('submissions'), $fileName);
 
-    GelombangSubmission::create([
-        'user_id' => Auth::id(),
-        'latihan_code' => $request->latihan_code,
-        'file_path' => $path
-    ]);
+        $path = 'submissions/' . $fileName;
 
-    return back()->with('success', 'File berhasil dikumpulkan!');
-}
+        GelombangSubmission::create([
+            'user_id' => Auth::id(),
+            'latihan_code' => $request->latihan_code,
+            'file_path' => $path
+        ]);
+
+        return back()->with('success', 'File berhasil dikumpulkan!');
+    }
 
     // =========================
     // HALAMAN GURU
